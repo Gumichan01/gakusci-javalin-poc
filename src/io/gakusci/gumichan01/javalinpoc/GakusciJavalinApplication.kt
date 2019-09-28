0b448a60d7f9579c237a156036991c93b1646e75
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-28 16:17:04 +0200

    Create hello world

--- src/io/gakusci/gumichan01/javalinpoc/GakusciJavalinApplication.kt
+++ src/io/gakusci/gumichan01/javalinpoc/GakusciJavalinApplication.kt
@@ -1,2 +1,8 @@
 package io.gakusci.gumichan01.javalinpoc
 
+import io.javalin.Javalin
+
+fun main() {
+    val app = Javalin.create().start(8080)
+    app.get("/") {ctx -> ctx.result("Hello world")}
+}
