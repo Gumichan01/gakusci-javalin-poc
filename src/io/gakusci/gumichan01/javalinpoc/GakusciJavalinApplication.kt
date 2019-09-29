372a107ef763195b35cc0585cb039a46bd2152d8
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-29 14:36:16 +0200

    Deal with exceptions and errors

--- src/io/gakusci/gumichan01/javalinpoc/GakusciJavalinApplication.kt
+++ src/io/gakusci/gumichan01/javalinpoc/GakusciJavalinApplication.kt
@@ -1,8 +1,18 @@
 package io.gakusci.gumichan01.javalinpoc
 
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
 
 fun main() {
-    val app = Javalin.create().start(8080)
-    app.get("/") {ctx -> ctx.result("Hello world")}
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, _ -> e.printStackTrace() }
+        error(404) { ctx -> ctx.json("not found") }
+    }.start(8080)
+    app.routes {
+        get("/") { ctx ->
+            ctx.result("""Hello world! 
+                |/!\ THSI IS A POC. DO NOT USE IT IN PRODUCTION /!\ """.trimMargin())
+        }
+    }
 }
+
