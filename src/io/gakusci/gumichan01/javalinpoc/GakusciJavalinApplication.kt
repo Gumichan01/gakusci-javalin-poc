00cc497c0e562f0e101d693637669dfe3a768d8b
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-09-29 15:28:58 +0200

    Deal with query parameters

--- src/io/gakusci/gumichan01/javalinpoc/GakusciJavalinApplication.kt
+++ src/io/gakusci/gumichan01/javalinpoc/GakusciJavalinApplication.kt
@@ -2,18 +2,28 @@ package io.gakusci.gumichan01.javalinpoc
 
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 fun main() {
+    val logger: Logger = LoggerFactory.getLogger("GakusciJavalinApplicationKt")
     val app = Javalin.create().apply {
         exception(Exception::class.java) { e, _ -> e.printStackTrace() }
         error(404) { ctx -> ctx.json("not found") }
-    }.start(8080)
+    }.start(8081)
     app.routes {
         get("/") { ctx ->
             ctx.result("""<p>Hello Gakusci POC client</p>
             <p>/!\ This webapp is a Proof of Concept /!\</p>
             <strong>DO NOT USE IT IN PRODUCTION</strong>""")
         }
+
+        get("/search/") { ctx ->
+            val resultString: String? = ctx.queryParam("q")
+            logger.info("query: $resultString")
+            resultString?.let { ctx.json(it) }
+        }
     }
 }
+
 
